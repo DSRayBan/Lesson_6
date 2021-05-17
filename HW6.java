15863104aeae9ea0ccad08acadd67caee92be5aa
DSRayBan <akkforxboxps@yandex.ru>
2021-05-17 19:11:20 +0300

    HW6

---
+++ HW6.java
@@ -0,0 +1,77 @@
+package ru.geekbrains.Lesson7;
+
+public class LessonSeven {
+    public static void main(String[] args) {
+        Cat[] cats = {
+                new Cat("Barsik", 10),
+                new Cat("Mirka", 15),
+                new Cat("Belok", 25),
+                new Cat("Tuzik", 5),
+                new Cat("Kuzya", 1)
+        };
+
+        Plate plate = new Plate(35);
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            cat.info();
+        }
+
+        plate.addFood(50);
+        plate.info();
+    }
+
+}
+
+
+
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean hungry;
+
+    Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.hungry = true;
+    }
+
+    void info() {
+        String isHungry = !hungry ? "сыт" : "голоден";
+        System.out.println(name + ": " + isHungry);
+    }
+
+    void eat(Plate plate) {
+        if (hungry && plate.decreaseFood(appetite))
+            hungry = false;
+    }
+}
+
+
+
+
+
+public class Plate {
+    private int food;
+
+    Plate(int food) {
+        this.food = food;
+    }
+
+    boolean decreaseFood(int n) {
+        int diff = food - n;
+        if (diff < 0) return false;
+
+        food -= n;
+        return true;
+    }
+
+    void addFood(int food) {
+        this.food += food;
+    }
+
+    void info() {
+        System.out.println("plate: " + food);
+    }
+}
